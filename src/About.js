433e4294bc75e97f4493112d632e76ed9ff9931c
mwikalikyalo <winniemwikali07@gmail.com>
2022-07-27 00:37:54 +0300

    Add the background image of the about page

---
+++ src/About.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Navbar from './Navbar';
+import './App.css';
+
+
+function About(){
+    return(       
+        <div>
+          <Navbar/>
+          <div className="container-fluid about">
+            <div className="bg-image"></div>
+              <div className="bg-text">
+                      <h1 style={{color: "#fca311"}}>About Me!</h1>
+                      <hr />
+                      <p>I consider myself a 'forever student' and a quick learner. I am eager to both build on my academic foundations in 
+                          Software Development with coding skills. I also have an educational background in Psychology and want to merge it 
+                          with the latest technological advancement through continued coursework and professional development. I am interested 
+                          in working with organizations that aim for building tech solutions, mentoring, training, and community building.</p>
+              </div>
+            </div>
+        </div>
+    );
+}
+
+export default About;
