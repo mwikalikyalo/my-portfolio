36c77f4555faf1ced903176241f30b2707b0a5f9
mwikalikyalo <winniemwikali07@gmail.com>
2022-07-27 01:41:09 +0300

    Add the basic footer contact us structure

--- src/Contact.js
+++ src/Contact.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import Navbar from './Navbar.js'
 
 function Contact(){
   return (
-     <div>
-        <Navbar/>
         <div className='container-fluid'>
+          <footer className="text-center text-lg-start">
+            <div className="container d-flex justify-content-center py-5">
+              <button type="button" className="btn btn-primary btn-lg btn-floating mx-2">
+                <i className="fab fa-facebook-f"></i>
+              </button>
+              <button type="button" className="btn btn-primary btn-lg btn-floating mx-2">
+                <i className="fab fa-youtube"></i>
+              </button>
+              <button type="button" className="btn btn-primary btn-lg btn-floating mx-2">
+                <i className="fab fa-instagram"></i>
+              </button>
+              <button type="button" className="btn btn-primary btn-lg btn-floating mx-2">
+                <i className="fab fa-twitter"></i>
+              </button>
+            </div>
 
+            <div className="text-center text-white p-3">
+              © 2022 Copyright:
+            </div>
+          </footer>
         </div>
-     </div>
   );
 } 
 
