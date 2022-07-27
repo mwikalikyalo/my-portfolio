79958377b2e0745ac724d6b5b93ce7ab56aa3150
mwikalikyalo <winniemwikali07@gmail.com>
2022-07-27 12:11:06 +0300

    Configure React app for deployment to GitHub Pages

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ function Home(){
             <div className="row">            
               <div className="d-flex justify-content-center start">
                 <div className="align-self-center intro">
-                  <img src={mypicture} className="mypicture img-fluid"/>
+                  <img src={mypicture} className="mypicture img-fluid" alt="girl smiling"/>
                   <h3 className="text-center">Winnie Mwikali's Portfolio</h3>
                 </div>
               </div>
